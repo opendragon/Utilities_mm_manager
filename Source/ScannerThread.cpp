a75c51757e9a5aa6e5ee28066290dbe09e57990b
johnty <johntywang@gmail.com>
2014-07-23 16:18:14 -0700

    release conf project settings for VS; temporary include check for yarp logger on Windows in ChannelManagerApp and ScannerThread

--- Source/ScannerThread.cpp
+++ Source/ScannerThread.cpp
@@ -60,7 +60,9 @@
 # pragma clang diagnostic ignored "-Wunused-parameter"
 # pragma clang diagnostic ignored "-Wweak-vtables"
 #endif // defined(__APPLE__)
+#if MAC_OR_LINUX_
 #include <yarp/os/impl/Logger.h>
+#endif //MAC_OR_LINUX_
 #if defined(__APPLE__)
 # pragma clang diagnostic pop
 #endif // defined(__APPLE__)
